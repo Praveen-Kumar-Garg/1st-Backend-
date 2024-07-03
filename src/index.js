9678a813e09d72342261935d3b337d8a875a9214
Praveen07garg <praveengarg9450@gmail.com>
2024-07-03 15:15:15 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,41 @@
+//require('dotenv').config9({path:'./env'})
+
+//import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+//database se jab bhi bat karo try catch lagao and async await
+
+//we can also import database by Immediately execute function
+//we can add semicolon while starting iiffe
+//import { DB_NAME } from "./constants";
+
+import connectDB from "./db/index.js";
+
+//this is due to the dotenv
+dotenv.config({
+    path: './env'
+})
+connectDB()
+//1st approach
+/*
+import express from "express"
+
+const app = express()
+
+    (async () => {
+        try {
+            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+            //we can add listener (express)
+            app.on("error", (error) => {
+                console.log("ERRR:", error);
+                throw error
+            })
+            app.listen(process.env.PORT , ()=>{
+                console.log(`app is listening on port ${process.env.PORT}`);
+            })
+        } catch (error) {
+            console.error("ERROR:", error)
+            throw error
+        }
+    })()
+        */
